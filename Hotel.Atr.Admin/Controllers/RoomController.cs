fb58f458c832e8fea920146e9b1d2e9a724e4246
СидоренкоЕ <@401_7>
2018-07-05 17:48:53 +0300

    Добавьте файлы проекта.

---
+++ Hotel.Atr.Admin/Controllers/RoomController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HotelAtr.DAL.Model;
+
+namespace Hotel.Atr.Admin.Controllers
+{
+    public class RoomController : BaseController
+    {
+
+        // GET: Room
+        public ActionResult Index(string message="")
+        {
+            ViewBag.Message = message;
+            return View(db.Rooms.ToList());
+
+        }
+        public ActionResult AddRoom(string action, Room room, int statusCode = -1)
+        {
+            ViewBag.RoomTypeList = db.RoomTypes.Select(s => new SelectListItem()
+            {
+                Text = s.Name,
+                Value = s.RoomTypeId.ToString()
+            }).ToList();
+            if(action=="Edit")
+            if (room.Square != null)
+                if (ServiceRoom.AddRoom(room))
+                    return RedirectToAction("Index", "Room");
+
+            ViewBag.StatusCode = 1;
+            return View();
+        }
+
+        public ActionResult EditRoom(int roomId)
+        {
+            Room findedRoom = db.Rooms.Find(roomId);
+            if (findedRoom != null)
+            {
+                try
+                {
+                    return RedirectToAction("Index", "Room", new {message = "Данные успешно изменены."});
+                }
+                catch (Exception e)
+                {
+                    return RedirectToAction("Index", "Room", new {message = e.ToString()});
+                }
+            }
+
+            return RedirectToAction("Index", "Room", new {message = "Данные пришли пустыми."});
+
+        }
+        public ActionResult DeleteRoom(int roomId)
+        {
+            if (ServiceRoom.DeleteRoom(roomId))
+            {
+                return RedirectToAction("Index", "Room", new { message = "Good Delete" });
+            }
+            return RedirectToAction("Index", "Room");
+        }
+    }
+}
---
+++ HotelAtr.DAL/Model/ServiceRoom.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelAtr.DAL.Model
+{
+    public class ServiceRoom
+    {
+        private static readonly HotelAtrEntities Db = new HotelAtrEntities();
+        public static bool AddRoom(Room room)
+        {
+            try
+            {
+                Db.Rooms.Add(room);
+                Db.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public static bool DeleteRoom(int roomId)
+        {
+            Room findedRoom = Db.Rooms.Find(roomId);
+            if (findedRoom != null)
+            {
+                try
+                {
+                    Db.Rooms.Remove(findedRoom);
+                    Db.SaveChanges();
+                    return true;
+                }
+                catch (Exception e)
+                {
+                    return false;
+                }
+            }
+
+            return false;
+        }
+
+    }
+}
